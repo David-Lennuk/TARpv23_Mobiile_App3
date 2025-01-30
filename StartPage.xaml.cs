e04ff5d58456466ece98ad07e12668b1b9a92cf4
opilane <opilane@HP-CZC2349HTZ>
2025-01-30 14:15:12 +0200

    Add project files.

---
+++ StartPage.xaml.cs
@@ -0,0 +1,43 @@
+namespace TARpv23_Mobiile_App;
+
+public partial class StartPage : ContentPage
+{
+    public List<ContentPage> lehed = new List<ContentPage>() { new TextPage(0), new FigurePage(1) };
+    public List<string> tekstid = new List<string> { "Tee lahti TekstPage", "Tee lahti FigurePage" };
+    ScrollView sv;
+    VerticalStackLayout vsl;
+    public StartPage()
+    {
+        Title = "Avaleht";
+        vsl = new VerticalStackLayout { BackgroundColor = Color.FromArgb("#FFC0CB") };
+        for (int i = 0; i < tekstid.Count; i++)
+        {
+            Button nupp = new Button
+            {
+                Text = tekstid[i],
+                BackgroundColor = Color.FromArgb("#EE82EE"),
+                TextColor = Color.FromArgb("#FF00FF"),
+                BorderWidth = 10,
+                ZIndex = i,
+                FontFamily = "Luckymoon 400",
+                FontSize = 30
+            };
+            vsl.Add(nupp);
+            nupp.Clicked += Lehte_avamine;
+        }
+        sv = new ScrollView { Content = vsl };
+        Content = sv;
+
+    }
+
+    private async void Lehte_avamine(object? sender, EventArgs e)
+    {
+        Button btn = (Button)sender;
+        await Navigation.PushAsync(lehed[btn.ZIndex]);
+    }
+
+    private async void Tagasi_Clicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new MainPage());
+    }
+}
---
+++ TextPage.xaml.cs
@@ -0,0 +1,99 @@
+namespace TARpv23_Mobiile_App;
+
+public partial class TextPage : ContentPage
+{
+    Label lbl;
+    Editor editor;
+    HorizontalStackLayout hsl;
+    List<string> buttons = new List<string> { "Tagasi", "Avaleht", "Edasi" };
+    Random rnd = new Random();
+    public TextPage(int k)
+    {
+        lbl = new Label
+        {
+            Text = "Pealkiri",
+            TextColor = Color.FromRgb(100, 10, 10),
+            FontFamily = "Luckymoon 400",
+            FontAttributes = FontAttributes.Bold,
+            TextDecorations = TextDecorations.Underline,
+            HorizontalTextAlignment = TextAlignment.Center,
+            VerticalTextAlignment = TextAlignment.Center,
+            FontSize = 28,
+        };
+
+        //??????? ?? ?????????
+        TapGestureRecognizer tapGestureRecognizer = new TapGestureRecognizer();
+        tapGestureRecognizer.Tapped += TapGestureRecognizer_Tapped;
+        lbl.GestureRecognizers.Add(tapGestureRecognizer);
+
+        editor = new Editor
+        {
+            Placeholder = "Vihje: Sisesta siia tekst",
+            PlaceholderColor = Color.FromRgb(250, 200, 100),
+            BackgroundColor = Color.FromRgb(200, 200, 100),
+            TextColor = Color.FromRgb(100, 50, 200),
+            FontSize = 28,
+            FontAttributes = FontAttributes.Italic,
+        };
+
+        editor.TextChanged += Teksti_sisestamine; // Lisame sündmus TextChanged, mis käivitab funktsiooni Tekste_sisestamine
+
+        hsl = new HorizontalStackLayout { };
+        for (int i = 0; i < 3; i++)
+        {
+            Button b = new Button
+            {
+                Text = buttons[i],
+                ZIndex = i,
+                WidthRequest = DeviceDisplay.Current.MainDisplayInfo.Width / 8.3,
+            };
+            hsl.Add(b);
+            b.Clicked += Liikumine; ;
+        }
+
+        VerticalStackLayout vst = new VerticalStackLayout
+        {
+            Children = { lbl, editor, hsl },
+            VerticalOptions = LayoutOptions.End
+        };
+
+        Content = vst;
+    }
+
+    private async void TapGestureRecognizer_Tapped(object? sender, TappedEventArgs e)
+    {
+        //????????? ??? ??????? ????????? ?? 360 ????????
+        await lbl.RelRotateTo(360, 500);
+        lbl.TextColor = Color.FromRgb(rnd.Next(256), rnd.Next(256), rnd.Next(256)); // ???????? ???? ????????? ??? ?????? ???????
+    }
+
+    private async void Liikumine(object? sender, EventArgs e)
+    {
+        Button btn = (Button)sender;
+        if (btn.ZIndex == 0)
+        {
+            await Navigation.PushAsync(new TextPage(btn.ZIndex));
+        }
+        else if (btn.ZIndex == 1)
+        {
+            await Navigation.PushAsync(new StartPage());
+        }
+        else
+        {
+            await Navigation.PushAsync(new FigurePage(btn.ZIndex));
+        }
+    }
+
+    private void Teksti_sisestamine(object? sender, TextChangedEventArgs e)
+    {
+        lbl.Text = editor.Text;
+
+        // ?????? ???? ????????? ? ??? ??? ?????
+        lbl.TextColor = Color.FromRgb(rnd.Next(256), rnd.Next(256), rnd.Next(256));
+        editor.BackgroundColor = Color.FromRgb(rnd.Next(256), rnd.Next(256), rnd.Next(256));
+    }
+    private async void Tagasi_Clicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new MainPage());
+    }
+}
